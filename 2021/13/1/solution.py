7997cf7ac24879fca70e175bf2921a29b26c190a
Peter O'Brien <pjo2412@gmail.com>
2021-12-13 02:07:07 -0500

    Completed 12/13/2021

---
+++ 2021/13/1/solution.py
@@ -0,0 +1,72 @@
+grid = []
+
+max_x = 0
+max_y = 0
+
+dots = []
+folds = []
+
+reading_folds = False
+
+def print_grid():
+    print()
+    for row in grid:
+        print(row)
+
+def fold(axis: str, val: int):
+    global grid
+    new_grid = []
+    if axis == 'y':
+        for r in range(0, val):
+            new_grid.append(grid[r])
+        for y in range(val+1, len(grid)):
+            for x in range(0, len(grid[y])):
+                if grid[y][x] == '#':
+                    new_grid[val - (y - val)][x] = '#'
+    else:
+        values_in_fold = []
+        for row in grid:
+            values_in_fold.append(row[val])
+            new_row = []
+            for x in range(0, val):
+                new_row.append(row[x])
+            for x in range(val+1, len(row)):
+                if row[x] == '#':
+                    new_row[val - (x - val)] = '#'
+            new_grid.append(new_row)
+    grid = new_grid
+
+with open('./input.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        if len(line.strip()) == 0:
+            reading_folds = True
+            continue
+        if reading_folds:
+            tokens = line.strip()[11:len(line.strip())].split('=')
+            folds.append((tokens[0], int(tokens[1])))
+        else:
+            dots.append(list(map(int, line.strip().split(','))))
+
+for dot in dots:
+    max_x = max(max_x, dot[0])
+    max_y = max(max_y, dot[1])
+
+for y in range(0, max_y+1):
+    row = []
+    for x in range(0, max_x+1):
+        row.append('.')
+    grid.append(row)
+
+for dot in dots:
+    grid[dot[1]][dot[0]] = '#'
+
+for f in folds:
+    fold(f[0], f[1])
+    break
+dot_count = 0
+for row in grid:
+    for dot in row:
+        if dot == '#':
+            dot_count += 1
+print(dot_count)
