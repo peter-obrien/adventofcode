e8fafa089080ddaf4160316d5b00cf678965b8c5
Peter O'Brien <pjo2412@gmail.com>
2021-12-20 14:46:18 -0500

    Swap matrix for dictionary of pixels

--- 2021/20/solution.py
+++ 2021/20/solution.py
@@ -3,73 +3,56 @@ import sys
 import time
 
 infinite_pixel = '.'
+PART_1_ENHANCEMENTS: int = 2
+PART_2_ENHANCEMENTS: int = 50
 
-def get_pixel(grid: list[list[str]], x:int, y: int):
-    if y < 0 or y >= len(grid):
-        return infinite_pixel
-    elif x < 0 or x >= len(grid[y]):
-        return infinite_pixel
-    else:
-        return grid[y][x]
-
-def get_neighbors_as_binary(grid: list[list[str]], x: int, y: int):
-    neighbors = [get_pixel(grid, x-1, y-1), get_pixel(grid, x, y-1), get_pixel(grid, x+1, y-1),
-            get_pixel(grid, x-1, y), get_pixel(grid, x, y), get_pixel(grid, x+1, y),
-            get_pixel(grid, x-1, y+1), get_pixel(grid, x, y+1), get_pixel(grid, x+1, y+1)]
+def get_neighbors_as_binary(grid: dict, x: int, y: int):
+    neighbors = [grid.get((x-1, y-1), infinite_pixel), grid.get((x, y-1), infinite_pixel), grid.get((x+1, y-1), infinite_pixel),
+            grid.get((x-1, y), infinite_pixel), grid.get((x, y), infinite_pixel), grid.get((x+1, y), infinite_pixel),
+            grid.get((x-1, y+1), infinite_pixel), grid.get((x, y+1), infinite_pixel), grid.get((x+1, y+1), infinite_pixel)]
     return ''.join(['0' if pixel == '.' else '1' for pixel in neighbors])
 
-def pad_grid(grid: list[list[str]]):
-    result = []
-    for _ in range(3):
-        result.append(['.' for _ in range(len(grid[0]) + 6)])
-    for row in grid:
-        new_row = ['.', '.', '.']
-        new_row.extend(row)
-        new_row.extend(['.', '.', '.'])
-        result.append(new_row)
-    for _ in range(3):
-        result.append(['.' for _ in range(len(grid[0]) + 6)])
-    return result
-
-def enhance_grid(grid: list[list[str]], enhancement_algo):
-    result = []
-    for y in range(len(grid)):
-        row = []
-        for x in range(len(grid[y])):
-            row.append(enhancement_algo[int(get_neighbors_as_binary(grid, x, y),2)])
-        result.append(row)
-    return result  
-
+def enhance_grid(pixels: dict, enhancement_algo, min_coord: tuple, max_coord: tuple):
+    new_pixels = dict()
+    for y in range(min_coord[1], max_coord[1]):
+        for x in range(min_coord[0], max_coord[0]):
+            new_pixels[(x,y)] = enhancement_algo[int(get_neighbors_as_binary(pixels, x, y), 2)]
+    return new_pixels
 
 if __name__ == '__main__':
     filename = './sample.txt' if '-s' in sys.argv else './input.txt'
     start_time = time.time()
 
     enhancement_algo = None
+    pixels = dict()
+    min_coord = (0,0)
+    max_coord = (0,0)
 
     with open(filename) as f:
         lines = f.read().splitlines()
         enhancement_algo = lines[0]
-        # If puzzle is a numerical matrix
-        matrix = [list(map(str, line)) for line in lines[2:]]
+        y: int = 0
+        for line in lines[2:]:
+            max_coord = (len(line)-1, y)
+            for x in range(len(line)):
+                pixels[(x,y)] = line[x]
+            y += 1
 
-    matrix = pad_grid(matrix)
-
-    for i in range(2):
+    for i in range(PART_1_ENHANCEMENTS):
         infinite_pixel = enhancement_algo[0] if i % 2 == 1 else '.'
-        matrix = enhance_grid(matrix, enhancement_algo)
-        if i % 2 == 1:
-            matrix = pad_grid(matrix)
+        min_coord = (min_coord[0]-2, min_coord[1]-2)
+        max_coord = (max_coord[0]+2, max_coord[1]+2)
+        pixels = enhance_grid(pixels, enhancement_algo, min_coord, max_coord)
 
-    result = sum([sum([1 if pixel == '#' else 0 for pixel in row]) for row in matrix])
+    result = sum([1 if pixel == '#' else 0 for pixel in pixels.values()])
     print(f"Part 1: {result} (took {(time.time() - start_time)}s)")
     start_time = time.time()
 
-    for i in range(48):
+    for i in range(PART_2_ENHANCEMENTS - PART_1_ENHANCEMENTS):
         infinite_pixel = enhancement_algo[0] if i % 2 == 1 else '.'
-        matrix = enhance_grid(matrix, enhancement_algo)
-        if i % 2 == 1:
-            matrix = pad_grid(matrix)
+        min_coord = (min_coord[0]-2, min_coord[1]-2)
+        max_coord = (max_coord[0]+2, max_coord[1]+2)
+        pixels = enhance_grid(pixels, enhancement_algo, min_coord, max_coord)
 
-    result = sum([sum([1 if pixel == '#' else 0 for pixel in row]) for row in matrix])
+    result = sum([1 if pixel == '#' else 0 for pixel in pixels.values()])
     print(f"Part 2: {result} (took {(time.time() - start_time)}s)")
