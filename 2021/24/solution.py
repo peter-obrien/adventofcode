4049435c082a0ea80b05f320bb7dbfb7ade8653c
Peter O'Brien <pjo2412@gmail.com>
2021-12-27 21:05:44 -0500

    Final WIP

--- 2021/24/solution.py
+++ 2021/24/solution.py
@@ -2,16 +2,10 @@ from __future__ import annotations
 from functools import lru_cache
 import sys
 import time
-from collections import deque
-from itertools import product
 
 class ALU:
     def __init__(self, instructions: list[tuple]) -> None:
-        self.vars = dict()
-        self.vars['w'] = 0
-        self.vars['x'] = 0
-        self.vars['y'] = 0
-        self.vars['z'] = 0
+        self.vars = {'w': 0, 'x': 0, 'y': 0, 'z': 0}
         self.instruction_sets = []
         counter = -1
         for i in instructions:
@@ -24,31 +18,31 @@ class ALU:
     def __str__(self) -> str:
         return f"w={self.vars['w']} | x={self.vars['x']} | y={self.vars['y']} | z={self.vars['z']}"
     
-    def execute(self, input: deque):
-        assert 14 == len(self.instruction_sets)
-        for iset in self.instruction_sets:
-            # print(iset)
-            self.vars['z'] = self.process_instruction_set(input.pop(), self.vars['z'], tuple(iset))
-            # print(f"{self.vars['z']} ->", end='')
-        # print()
-    
     @lru_cache(maxsize=None)
-    def execute_dfs(self, depth: int = 0, z: int = 0, val: int = 0):
-        result = []
-        for i in range(1,10):
-            z = self.process_instruction_set(i, z, self.instruction_sets[depth])
+    def execute_dfs(self, depth: int = 0, z: int = 0, find_max: bool = True):
+        search_range = range(9, 0, -1) if find_max else range(1, 10)
+        for i in search_range:
+            if depth == 0:
+                # Clear the cache after each top level number to keep memory consumption semi bounded
+                print(f'Searching Depth {depth} - Value{i}. Clearing caches! {time.ctime()}')
+                self.execute_dfs.cache_clear()
+                self.process_instruction_set.cache_clear()
+            z_for_i = self.process_instruction_set(i, z, depth)
             if depth == 13:
-                return [] if z != 0 else [val]
+                if z_for_i == 0:
+                    return str(i)
             else:
-                result.extend(self.execute_dfs(depth+1, z, (val*10)+i))
-        return result
-    
+                depth_result = self.execute_dfs(depth + 1, z_for_i, find_max)
+                if depth_result:
+                    return str(i) + depth_result
+        return None
+
     # All instruction sets start with an input into 'w' and 'x' and 'y' are cleared to 0 via mul operations so 'z' is the only carry over
-    # @lru_cache(maxsize=None)
-    def process_instruction_set(self, input: int, z: int, instructions: list[tuple]) -> int:
+    @lru_cache(maxsize=None)
+    def process_instruction_set(self, input: int, z: int, depth: int) -> int:
         self.next_input = input
         self.vars['z'] = z # Unnecessary, but wanted to use the variable
-        for i in instructions:
+        for i in self.instruction_sets[depth]:
             self.process_instruction(i)
         return int(self.vars['z'])
 
@@ -77,62 +71,9 @@ if __name__ == '__main__':
     with open(filename) as f:
         instructions = list(map(tuple, [line.split() for line in f.read().splitlines()]))
 
-    max_model_num = 0
-    min_model_num = sys.maxsize
     P = ALU(instructions)
 
-    print(P.execute_dfs())
-
-    # print(P.process_instruction_set(9, 0, tuple(P.instruction_sets[0])))
-
-    # Goal is for z == 0 at end
-    # instruction_solutions = dict()
-    # goal = [0]
-    
-    # for i in range(13, -1, -1):
-    #     new_goal = []
-    #     instruction_solutions[i-1] = dict()
-    #     print(f"Instruction {i}")
-    #     # print(P.instruction_sets[i])
-    #     for w in range(1,10):
-    #         target_z = []
-    #         print(f"w={w} z needs to be: ", end='')
-    #         for z in range(100000):
-    #             if P.process_instruction_set(w, z, tuple(P.instruction_sets[i])) in goal:
-    #                 print(f"{z},", end='')
-    #                 new_goal.append(z)
-    #         print()
-    #         instruction_solutions[i-1][w] = target_z
-    #     goal = new_goal
-    # instruction_solutions = dict()
-    # instruction_solutions[13] = {1: [0], 2: [0], 3: [0], 4: [0], 5: [0], 6: [0], 7: [0], 8: [0], 9: [0]}
-    # for i in range(13, 11, -1):
-    #     instruction_solutions[i-1] = dict()
-    #     print(f"Instruction {i}")
-    #     # print(P.instruction_sets[i])
-    #     for w in range(1,10):
-    #         target_z = []
-    #         for z in range(100000):
-    #             if P.process_instruction_set(w, z, tuple(P.instruction_sets[i])) in instruction_solutions[i]:
-    #                 print(z)
-    #                 # target_z.append(z)
-    #         instruction_solutions[i-1][w] = target_z
-    # for ins in instruction_solutions:
-    #     print(f"Instruction {ins}")
-    #     for w in instruction_solutions[ins]:
-    #         print(f"w={w} z needs to be {instruction_solutions[ins][w]}")
-
-    # for a,b,c,d,e,f,g,h,i,j,k,l,m,n in product(range(4,5), repeat=14):
-    #     P.execute(deque((a,b,c,d,e,f,g,h,i,j,k,l,m,n)))
-    #     val = int(f"{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}{m}{n}")
-    #     if P.vars['z'] == 0:
-    #         max_model_num = max(max_model_num, val)
-    #         min_model_num = min(min_model_num, val)
-    #         print(f"min is now = {min_model_num} | max is now = {max_model_num}")
-    #     else:
-    #         print(f"Rejected: {val}")
-
-    print(f"Part 1: {max_model_num} (took {(time.time() - start_time)}s)")
+    print(f"Part 1: {P.execute_dfs()} (took {(time.time() - start_time)}s)")
     start_time = time.time()
 
-    print(f"Part 2: {min_model_num} (took {(time.time() - start_time)}s)")
+    print(f"Part 2: {P.execute_dfs(find_max=False)} (took {(time.time() - start_time)}s)")
